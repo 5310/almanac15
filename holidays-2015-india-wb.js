1956b2209025c59bab1542f0f73b6aabee5867c5
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-12-24 18:47:50 +0530

    India-WB holidays for 2015 updated.

--- holidays-2015-india-wb.js
+++ holidays-2015-india-wb.js
@@ -3,46 +3,52 @@
 holidays['2015'] = {
 	'india-wb': {
 		'0': {
-			'23': "Netaji's B'Day",
-			'24': "Vasanta Panchami",
+			'4': "Fateha-e-Dwaj Daham",
+			'12': "Vivekananda's B'day",
+			'23': "Netaji's B'day",
 			'25': "Saraswati Puja",
-			'26': "Republic Day"
+			'26': "Republic Day",
 		},
 		'2': {
-			'5': "Doljatra"
+			'5': "Doljatra",
 		},
 		'3': {
 			'1': "Bank Holiday",
 			'3': "Good Friday",
+			'14': "Ambedkar's B'day",
 			'15': "Bengali New year"
 		},
 		'4': {
-			'1': "May Day"
+			'1': "May Day",
+			'9': "Tagore's B'day"
 		},
 		'6': {
-			'18': "Idu'l Fitr"
+			'18': "Eid al-Fitr"
 		},
 		'7': {
 			'15': "Independence Day"
 		},
 		'8': {
-			'23': "Idu'l Zuha",
-			'30': "Bank Holiday"
+			'5': "Janmashtami",
+			'25': "Eid al-Zuha"
 		},
 		'9': {
 			'2': "Gandhi's B'day",
-			'12': "Good Friday",
+			'12': "Mahalaya",
+			'19': "Durgasashthi",
 			'20': "Mahasaptami",
-			'21': "Mahaashtami & Mahanavami",
-			'22': "Vijaya Dashami",
+			'21': "Mahaashtami",
+			'22': "Mahanavami & Vijayadashami",
 			'24': "Muharram",
 			'26': "Lakshmi Puja"
 		},
 		'10': {
 			'10': "Kali Puja",
-			'30': "Guru Nanak's B'day"
+			'13': "Bhatridwitiya",
+			'25': "Guru Nanak's B'day"
 		},
 		'11': {
+			'24': "Fateha-e-Dwaj Daham",
 			'25': "Christmas Day"
 		},
 	}
